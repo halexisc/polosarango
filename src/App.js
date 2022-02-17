1c8385586477b01566d4d035aa904b4eed01b561
halexisc <halexisc@gmail.com>
2022-02-17 10:28:26 -0500

    se realiza actualización para entrega intermedia

--- src/App.js
+++ src/App.js
@@ -6,11 +6,21 @@ import CartWidget from './components/CartWidget';
 import ItemList from './components/ItemList';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import ItemDetail from './components/ItemDetail'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 function App() {
-  return (
-    <>
-    <Navbar />
+  return (<>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" alement={<ItemListContainer />} />
+      <Route path="/Item" alement={<ItemDetailContainer />} />
+      <Route path="/price/:" alement={<ItemDetailContainer />} />
+    </Routes>
+    
+    
+    </BrowserRouter>
+
    
   {<ItemListContainer      />}
     {/*<ItemDetailContainer />*/}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as bootstrap from 'bootstrap';
 import CartWidget from './CartWidget'; 
+import { Link } from 'react-router-dom'
+
+
+
 
 const Navbar = () => {
     return (
           
       <ul className="nav">
       <li className="nav-item">
-        <a className="nav-link active" aria-current="page" href="#">Polos Arango.</a>
+          <Link to='/' ><a className="nav-link active" aria-current="page" href="#">Polos Arango.</a></Link>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="#">Inicio</a>
       </li>
       <li className="nav-item">
-        <a class="nav-link" href="#">Nosotros</a>
+      <Link to='/category' > <a class="nav-link" href="#">Categoria</a></Link>
+      </li>
+      <li className="nav-item">
+      <Link to='/price' > <a class="nav-link" href="#">Precio</a></Link>
       </li>
       <li className="nav-item">
         <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Catáctanos</a>
