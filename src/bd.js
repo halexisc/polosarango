84de036c236a131c619994f58afdc15c53eda2ea
halexisc <halexisc@gmail.com>
2022-02-18 13:37:01 -0500

    corrección estilos

--- src/bd.js
+++ src/bd.js
@@ -5,31 +5,31 @@ export const list = [{
     id: "01",
     title: "polo negra",
     price: 50,
-    pictureurl: "https://static.dafiti.com.co/p/hamer-7171-789348-1-zoom.jpg"
+    pictureurl: "../public/polo6.jpg"
 
 },{
     id: "02",
     title: "polo azul",
     price: 50,
-    pictureurl: "https://static.dafiti.com.co/p/hamer-7171-789348-1-zoom.jpg"
+    pictureurl: "../public/polo6.jpg"
 
 },{
     id: "03",
     title: "polo blanca",
     price: 50,
-    pictureurl: "https://static.dafiti.com.co/p/hamer-7171-789348-1-zoom.jpg"
+    pictureurl: "../public/polo6.jpg"
 
 },{
     id: "04",
     title: "polo verde",
     price: 50,
-    pictureurl: "https://static.dafiti.com.co/p/hamer-7171-789348-1-zoom.jpg"
+    pictureurl: "../public/polo6.jpg"
 
 },{
     id: "01",
     title: "polo surf",
     price: 50,
-    pictureurl: "https://static.dafiti.com.co/p/hamer-7171-789348-1-zoom.jpg"
+    pictureurl: "../public/polo6.jpg"
 
 }];
 
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,21 +1,24 @@
-import * as bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'bootstrap';
+import '../components/estylo.css'
 
 export default function Item({id, title, price, pictureurl}){
     return(
 <>
-       
 
-      <article>
-              <span>{id}</span>
-              <span>{title}</span>
-              <span>{price}</span>
-                <figure>
-                    <img src={pictureurl} alt={id} /> 
-                </figure>
-
-       </article> 
+      <div className="card" styleName="width: 18rem;" >
+      <figure>
+                    <img className="card-img-top"  src={pictureurl} alt={id} /> 
+      </figure>
+        
+       <div className="card-body">
+        <h5 className="card-title">Prenda: {title}</h5>
+       <p className="card-text">Precio: {price}</p>
+       <h5 className="card-title">Cod: {id}</h5>
+       <a href="#" className="btn btn-primary">Agregar al carrito</a>
+        </div>
+       </div> 
+     
    </>
     
     )
